52e2a96be79f225dd35a20549b154ff01dcc4b06
dwijeshMishra <60146522+DwijeshMishra@users.noreply.github.com>
2021-04-29 16:50:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ resource "google_service_account" "sa-name" {
   display_name = "test"
 }
 
-resource "google_project_iam_member" "firestore_owner_binding" {
+resource "google_project_iam_member" "create_member" {
   project = "asnbile"
   role    = "roles/compute.viewer"
   member  = "serviceAccount:${google_service_account.sa-name.email}"
